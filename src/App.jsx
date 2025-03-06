524dca5a4e11630c525ba344cab4d7b04c91d19d
vivekkumar2004-coder-code <vivekkrsahu2004@gmail.com>
2025-03-06 23:45:23 +0530

    "Added components and required pages"

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
 import React from "react";
+import {Route, Routes } from 'react-router-dom'
+import ContactUs from "./pages/ContactUs";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Login from "./pages/Login";
+import Myprofile from "./pages/Myprofile";
+import MyProducts from "./pages/MyProducts";
+import BuyProduct from "./pages/BuyProduct";
+
+import Navbar from "./components/Navbar";
+
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div>
-      <div className="header bg-green-300 text-blue-600">header</div>
+    <div className='mx-4 sm:mx-[10%]'>
+     <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/Products' element={<Products/>} />
+      <Route path='/products/:specialproducts' element={<Products/>} />
+      <Route path='/login' element={<Login/>}/>
+     
+      <Route path='/contact'element={<ContactUs/>}/>
+      <Route path='/my-profile' element={<Myprofile/>}/>
+      <Route path='/my-products' element={<MyProducts/>}/>
+      <Route path='/Buyproduct/:productid' element={<BuyProduct/>}/>
+
+      
+
+    </Routes>
+    <Footer/>
+     
     </div>
   );
 };
---
+++ src/pages/RequestProduct.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RequestProduct = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default RequestProduct
