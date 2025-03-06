6599545be0e2e5de848aa5e063ffb13b6536da1f
vivekkumar2004-coder-code <vivekkrsahu2004@gmail.com>
2025-03-06 21:44:27 +0530

    "configured react vite tailwind"

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <div className="header bg-green-300 text-blue-600">header</div>
+    </div>
+  );
+};
+
+export default App;
